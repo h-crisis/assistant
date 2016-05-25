d9c60affad54c8e6383f774d30eda77ac424985e
KKomor <kenichiro.komori@hcrisis2.onmicrosoft.com>
2016-05-25 10:27:40 +0900

    commit
    commit massage

--- test/html/komori/template/si.js
+++ test/html/komori/template/si.js
@@ -1,10 +1,9 @@
 /**
  * Created by manabu on 2016/05/19.
  */
-
-var styleSI7 = new ol.style.Style({
+var styleSI1 = new ol.style.Style({
     fill: new ol.style.Fill({
-        color: 'rgba(180, 0, 104, 0.5)'
+        color: 'rgba(242, 242, 255, 0.5)'
     }),
     image: new ol.style.Icon({
         scale: 1,
@@ -12,85 +11,7 @@ var styleSI7 = new ol.style.Style({
         anchorXUnits: 'fraction',
         anchorYUnits: 'fraction',
         opacity: 0.85,
-        src: "7.png"
-    })
-});
-var styleSI6S = new ol.style.Style({
-    fill: new ol.style.Fill({
-        color: 'rgba(165, 0, 33, 0.5)'
-    }),
-    image: new ol.style.Icon({
-        scale: 1,
-        anchor: [0.5, 1],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'fraction',
-        opacity: 0.85,
-        src: "6s.png"
-    })
-});
-var styleSI6W = new ol.style.Style({
-    fill: new ol.style.Fill({
-        color: 'rgba(255, 40, 0, 0.5)'
-    }),
-    image: new ol.style.Icon({
-        scale: 1,
-        anchor: [0.5, 1],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'fraction',
-        opacity: 0.85,
-        src: "6w.png"
-    })
-});
-var styleSI5S = new ol.style.Style({
-    fill: new ol.style.Fill({
-        color: 'rgba(255, 153, 0, 0.5)'
-    }),
-    image: new ol.style.Icon({
-        scale: 1,
-        anchor: [0.5, 1],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'fraction',
-        opacity: 0.85,
-        src: "5s.png"
-    })
-});
-var styleSI5W = new ol.style.Style({
-    fill: new ol.style.Fill({
-        color: 'rgba(255, 230, 0, 0.5)'
-    }),
-    image: new ol.style.Icon({
-        scale: 1,
-        anchor: [0.5, 1],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'fraction',
-        opacity: 0.85,
-        src: "5w.png"
-    })
-});
-var styleSI4 = new ol.style.Style({
-    fill: new ol.style.Fill({
-        color: 'rgba(255, 230, 150, 0.5)'
-    }),
-    image: new ol.style.Icon({
-        scale: 1,
-        anchor: [0.5, 1],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'fraction',
-        opacity: 0.85,
-        src: "4.png"
-    })
-});
-var styleSI3 = new ol.style.Style({
-    fill: new ol.style.Fill({
-        color: 'rgba(0, 65, 255, 0.5)'
-    }),
-    image: new ol.style.Icon({
-        scale: 1,
-        anchor: [0.5, 1],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'fraction',
-        opacity: 0.85,
-        src: "3.png"
+        src: "1.png"
     })
 });
 var styleSI2 = new ol.style.Style({
@@ -106,9 +27,9 @@ var styleSI2 = new ol.style.Style({
         src: "2.png"
     })
 });
-var styleSI1 = new ol.style.Style({
+var styleSI3 = new ol.style.Style({
     fill: new ol.style.Fill({
-        color: 'rgba(242, 242, 255, 0.5)'
+        color: 'rgba(0, 65, 255, 0.5)'
     }),
     image: new ol.style.Icon({
         scale: 1,
@@ -116,7 +37,85 @@ var styleSI1 = new ol.style.Style({
         anchorXUnits: 'fraction',
         anchorYUnits: 'fraction',
         opacity: 0.85,
-        src: "1.png"
+        src: "3.png"
+    })
+});
+var styleSI4 = new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: 'rgba(255, 230, 150, 0.5)'
+    }),
+    image: new ol.style.Icon({
+        scale: 1,
+        anchor: [0.5, 1],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+        opacity: 0.85,
+        src: "4.png"
+    })
+});
+var styleSI5W = new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: 'rgba(255, 230, 0, 0.5)'
+    }),
+    image: new ol.style.Icon({
+        scale: 1,
+        anchor: [0.5, 1],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+        opacity: 0.85,
+        src: "5w.png"
+    })
+});
+var styleSI5S = new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: 'rgba(255, 153, 0, 0.5)'
+    }),
+    image: new ol.style.Icon({
+        scale: 1,
+        anchor: [0.5, 1],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+        opacity: 0.85,
+        src: "5s.png"
+    })
+});
+var styleSI6W = new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: 'rgba(255, 40, 0, 0.5)'
+    }),
+    image: new ol.style.Icon({
+        scale: 1,
+        anchor: [0.5, 1],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+        opacity: 0.85,
+        src: "6w.png"
+    })
+});
+var styleSI6S = new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: 'rgba(165, 0, 33, 0.5)'
+    }),
+    image: new ol.style.Icon({
+        scale: 1,
+        anchor: [0.5, 1],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+        opacity: 0.85,
+        src: "6s.png"
+    })
+});
+var styleSI7 = new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: 'rgba(180, 0, 104, 0.5)'
+    }),
+    image: new ol.style.Icon({
+        scale: 1,
+        anchor: [0.5, 1],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+        opacity: 0.85,
+        src: "7.png"
     })
 });
 
@@ -130,24 +129,24 @@ var layerSI = new ol.layer.Vector({
         format: new ol.format.GeoJSON()
     }),
     style: function(feature, resolution) {
-        if(feature.get('SI')>=6.5) {
-            return styleSI7;
-        } else if(feature.get('SI')>=6) {
-            return styleSI6S;
-        } else if(feature.get('SI')>=5.5) {
-            return styleSI6W;
-        } else if(feature.get('SI')>=5) {
-            return styleSI5S;
-        } else if(feature.get('SI')>=4.5) {
-            return styleSI5W;
-        } else if(feature.get('SI')>=3.5) {
-            return styleSI4;
-        } else if(feature.get('SI')>=2.5) {
-            return styleSI3;
-        } else if(feature.get('SI')>=1.5) {
-            return styleSI2;
-        } else if(feature.get('SI')>0) {
+        if(feature.get('SI')<1.5) {
             return styleSI1;
+        } else if(feature.get('SI')<2.5) {
+            return styleSI2;
+        } else if(feature.get('SI')<3.5) {
+            return styleSI3;
+        } else if(feature.get('SI')<4.5) {
+            return styleSI4;
+        } else if(feature.get('SI')<5) {
+            return styleSI5W;
+        } else if(feature.get('SI')<5.5) {
+            return styleSI5S;
+        } else if(feature.get('SI')<6) {
+            return styleSI6W;
+        } else if(feature.get('SI')<6.5) {
+            return styleSI6S;
+        } else if(feature.get('SI')>=6.5) {
+            return styleSI7;
         } else {}
     }
 });
