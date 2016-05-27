96e6564a074011d796ac620ff07e1077c0f9b03a
Manabu Ichikawa <manabu.ichikawa@ichilab.org>
2016-05-27 16:08:54 +0900

    震度を境界に紐付ける2016.5.26
    岩崎くん向けにファイルを更新

--- test/ichikawa/openShapeFile.java
+++ test/ichikawa/openShapeFile.java
@@ -2,8 +2,11 @@ package ichikawa;
 
 import org.geotools.data.FileDataStore;
 import org.geotools.data.FileDataStoreFinder;
-import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.feature.FeatureIterator;
+import org.opengis.feature.Feature;
 import org.opengis.feature.simple.SimpleFeature;
 
 import java.io.File;
@@ -13,14 +16,36 @@ import java.io.File;
  */
 public class openShapeFile {
     public static void main(String args[]) throws Exception {
+        // Shapeファイルを指定する
         File file = new File("files/ShapeFiles/municipalities/municipalities.shp");
 
+        // ShapeファイルをDataStoreに紐付け、DataStoreをFeatureSourceに関連づける
         FileDataStore store = FileDataStoreFinder.getDataStore(file);
+        FeatureSource featureSource = store.getFeatureSource();
+
+        // FeatureSourceから全Featureを要素にもつコレクションが取れる
+        FeatureIterator i = featureSource.getFeatures().features();
+        while(i.hasNext())
+            System.out.println(((Feature) i.next()).getProperties());
+    }
+
+    public void openShapeFile2() throws Exception{
+        // Shapeファイルを指定する
+        File file = new File("files/ShapeFiles/municipalities/municipalities.shp");
+
+        // ShapeファイルをDataStoreに紐付け、DataStoreをFeatureSourceに関連づける
+        FileDataStore store = FileDataStoreFinder.getDataStore(file);
+
+
+        // storeからSimpleFeatureSourceオブジェクトを作成する。
         SimpleFeatureSource featureSource = store.getFeatureSource();
 
-        SimpleFeatureIterator i = featureSource.getFeatures().features();
+        // FeatureSourceから全Featureを要素にもつコレクションが取り、イテレーターを定義する。
+        SimpleFeatureCollection c = featureSource.getFeatures();
+        FeatureIterator i = featureSource.getFeatures().features();
         while(i.hasNext()) {
-            System.out.println(((SimpleFeature)i.next()).getAttributes().size());
+            SimpleFeature feature = (SimpleFeature)i.next();
+            System.out.println(((SimpleFeature) i.next());
         }
     }
 }
