ee66381cd44bfbef713166913495269a930ad0d3
Manabu Ichikawa <manabu.ichikawa@ichilab.org>
2016-08-23 14:59:43 +0900

    20160823 GeoCodingクラスの生成

---
+++ Common/src/gis/GeoCoding.java
@@ -0,0 +1,42 @@
+package gis;
+
+import com.google.code.geocoder.Geocoder;
+import com.google.code.geocoder.GeocoderRequestBuilder;
+import com.google.code.geocoder.model.GeocodeResponse;
+import com.google.code.geocoder.model.GeocoderRequest;
+import com.google.code.geocoder.model.GeocoderResult;
+
+import java.io.IOException;
+import java.util.List;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * Created by manab on 2016/07/23.
+ */
+public class GeoCoding {
+
+    /**
+     * 指定した住所、ランドマークのGeoCoding結果を返す。
+     * @param string GeoCodingしたい文字列
+     * @param language GeoCodingする文字列の言語（en, jp, など）
+     * @return GeoCoding結果のリストを返す
+     */
+    public static List<GeocoderResult> getGeocoderResult(String string, String language) {
+        final Geocoder geocoder = new Geocoder();
+        GeocoderRequest geocoderRequest = new GeocoderRequestBuilder().setAddress(string).setLanguage(language).getGeocoderRequest();
+        try {
+            sleep(1000);
+            GeocodeResponse geocoderResponse = geocoder.geocode(geocoderRequest);
+            List<GeocoderResult> results = geocoderResponse.getResults();
+            return results;
+        } catch (IOException e) {
+            System.out.println("Google GeoCoding中にエラーが生じました。");
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            System.out.println("Google GeoCoding中にエラーが生じました。");
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- DisasterDamageEstimate/src/Main.java
+++ DisasterDamageEstimate/src/Main.java
@@ -8,9 +8,9 @@ public class Main {
     public static void main(String args[]) throws Exception {
         File masterFilesDir = new File("files/master");
         File shapeDir = new File("files/shape");
-        File siFile = new File("files/JSHIS/F007701/S-V2-F007701-MAP-CASE1.csv");
+        File siFile = new File("files/JSHIS/NankaiKochi/nankai_kochi.csv");
         File outDir = new File("files/out");
-        String code = "F007701";
+        String code = "NankaiKochi";
 
         new EarthquakeDamageEstimate(masterFilesDir, shapeDir, siFile, outDir, code);
     }
