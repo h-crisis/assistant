e69f3f2d0c1b3c00e30a6a5cf5d32a9755c8c2ce
K2 <komori10@cs.dis.titech.ac.jp>
2016-10-15 00:24:48 +0900

    [fix]避難所入力シート保健師長会式の更新

---
+++ Webpage/template/js/mqttConfig.js
@@ -0,0 +1,23 @@
+/**
+ * Created by K2 on 2016/10/14.
+ */
+
+// host = '172.16.153.122';	// hostname or IP address
+host = 'broker.h-crisis.jp';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+port = 80;
+topic = '#';		// topic to subscribe to
+useTLS = false;
+username = null;
+password = null;
+// username = "jjolie";
+// password = "aa";
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
