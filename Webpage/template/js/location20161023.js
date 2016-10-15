3dad67940d7de7b16def9bc4b450397ebbb450d0
K2 <komori10@cs.dis.titech.ac.jp>
2016-10-15 16:02:55 +0900

    [fix]

---
+++ Webpage/template/js/location20161023.js
@@ -0,0 +1,68 @@
+/**
+ * Created by manabu on 2016/09/07.
+ * 位置情報全般のJavaScript
+ */
+
+
+// デフォルトの中心に移動する
+var goCenter = document.getElementById('goToHypoCenter');
+goCenter.addEventListener('click', goHypoButton);
+function goHypoButton() {
+    var pan = ol.animation.pan({
+        duration: 2000,
+        source: (view.getCenter())
+    });
+    map.beforeRender(pan);
+    map.getView().setCenter(hypoPoint);
+    map.getView().setZoom(11);
+}
+
+// Webブラウザから取得した現在地に移動する
+var goHere = document.getElementById('goToHere');
+goHere.addEventListener('click', goToHere);
+function goToHere() {
+    var nowLatLon = document.getElementById('latlonCurr').value;
+    if (nowLatLon === ""){
+        alert("現在地が取得されていません。")
+    } else {
+        nowLatLon = nowLatLon.split(',');
+        var latNow = nowLatLon[1].substr(0, 8);
+        var lonNow = nowLatLon[0].substr(0, 8);
+        // nowLatLon = ol.proj.transform([lonNow, Math.abs(latNow)], 'EPSG:4326', 'EPSG:3857');
+        var start = +new Date();
+        var pan = ol.animation.pan({
+            duration: 2000,
+            source: (view.getCenter()),
+            start: start
+        });
+        var bounce = ol.animation.bounce({
+            duration: 2000,
+            resolution: 4 * view.getResolution(),
+            start: start
+        });
+        map.beforeRender(pan, bounce);
+        map.getView().setCenter(nowLatLon);
+        map.getView().setZoom(15);
+    }
+}
+
+//　eventで設定した中心地に移動する
+var goHypocenter = document.getElementById('goToHypoCenter');
+goHypocenter.addEventListener('click', goToHypo);
+function goToHypo() {
+    var centerLatLon = [centerLat,centerLon];
+    var start = +new Date();
+    var pan = ol.animation.pan({
+        duration: 2000,
+        source: (view.getCenter()),
+        start: start
+    });
+    var bounce = ol.animation.bounce({
+        duration: 2000,
+        resolution: 4 * view.getResolution(),
+        start: start
+    });
+    map.beforeRender(pan, bounce);
+    map.getView().setCenter(centerLatLon);
+    map.getView().setZoom(15);
+}
---
+++ Webpage/template/js/pass20161023.js
@@ -0,0 +1,62 @@
+/**
+ * Created by komori on 2016/07/20.
+ */
+
+var styleImpassable = new ol.style.Style({
+    text: new ol.style.Text({
+        font: '12px Calibri,sans-serif',
+        fill: new ol.style.Fill({color: "#000000"}),
+        stroke: new ol.style.Stroke({color: "#ffff00", width: 2})
+    }),
+    image: new ol.style.Icon({
+        scale: 1,
+        anchor: [0.5, 0.5],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'fraction',
+        opacity: 1,
+        src: 'http://h-crisis.niph.go.jp/assistant/wp-content/uploads/sites/4/test/img/X.png'
+    })
+});
+
+var urlP = 'http://h-crisis.niph.go.jp/assistant/wp-content/uploads/data/road_info/latest/road_info.geojson';
+
+var ImpassableLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: urlP,
+        format: new ol.format.GeoJSON()
+    }),
+    style: function(feature) {
+        styleImpassable.getText().setText(feature.get('name') +  ":" + feature.get('規制内容') + "(理由:" +　feature.get('規制原因') + ")");
+        return styleImpassable;
+    }
+});
+
+function visPassButton() {
+    if (ImpassableLayer.getVisible()) {
+        ImpassableLayer.setVisible(false);
+        this.style.backgroundColor = "whitesmoke";
+    } else {
+        ImpassableLayer.setVisible(true);
+        this.style.backgroundColor = "red";
+    }
+};
+
+// 交通規制のポップアップを作成する関数
+function createHtmlPass() {
+    document.getElementById('info').innerHTML = "";
+    DetailHtml = "";
+    HeaderHtml = "<div style='border:2px solid burlywood; background-color:#888888; color:white; text-align:center' type=button id=showBtn value=隠す onclick=showHide()>閉じる</div>";
+
+    // 建築物名表示
+    if (arrayV[0] === null) {
+    } else {
+        HeaderHtml = HeaderHtml + arrayV[0];
+    }
+    for (i = 0; i < arrayL.length; i++) {
+        DetailHtml = DetailHtml + preCells + arrayL[i] + interCells + arrayV[i] + subCells;
+    }
+    document.getElementById('infoHeader').style.display = 'block';
+    document.getElementById('infoHeader').innerHTML = HeaderHtml;
+    document.getElementById('info').style.display = 'block';
+    document.getElementById('info').innerHTML = DetailHtml;
+}
