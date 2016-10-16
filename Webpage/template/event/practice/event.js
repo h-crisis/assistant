0d00c3e647c46a2e5d4ce4c26804006794b66dea
Manabu Ichikawa <manabu.ichikawa@ichilab.org>
2016-10-17 01:43:52 +0900

    201610170143 Webpage templateの更新
    震度分布で、必要項目だけが表示されるようにhalls20161023.jsを更新。

---
+++ Webpage/template/event/practice/event.js
@@ -0,0 +1,17 @@
+/**
+ * Created by komori on 2016/08/12.
+ */
+
+// eventCodeと経度（lat)と緯度（lon）を指定する
+var eventCode = 'practice';
+var centerLat = 139.620707;
+var centerLon = 35.790259;
+var arrayL = new Array(0);
+var arrayV = new Array(0);
+
+var shelterLayer = 'event_shelter_' + eventCode;
+
+var pass = './'
+//var pass = 'http://h-crisis.niph.go.jp/wp-content/uploads/sites/4/event/' + eventCode + '/';
+var wms = 'http://gis.h-crisis.jp/geoserver/wms';
+//var wms = 'http://map.ichilab.org:80/geoserver/wms';
