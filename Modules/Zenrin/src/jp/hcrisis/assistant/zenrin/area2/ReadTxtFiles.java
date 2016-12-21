3c08b3ddca514073f635566ea4ad3d453567431b
Manabu Ichikawa <manabu.ichikawa@ichilab.org>
2016-12-21 22:03:31 +0900

    2016.12.20 22:03 Zenrinモジュールの更新

--- Modules/Zenrin/src/jp/hcrisis/assistant/zenrin/area2/ReadTxtFiles.java
+++ Modules/Zenrin/src/jp/hcrisis/assistant/zenrin/area2/ReadTxtFiles.java
@@ -23,11 +23,11 @@ import java.util.concurrent.Executors;
  */
 public class ReadTxtFiles {
     public static void main(String args[]) throws Exception {
-        File dir = new File("/Users/manabu/Desktop/ZenrinOUT/TEST");
-        //File dir = new File("C:\\Users\\manab\\Dropbox\\GIS\\GISデータ\\ゼンリン\\Area Map2\\OUT\\TXT");
+        File file = new File("/Users/manabu/Desktop/ZenrinOUT/TXT/9.txt");
+        File dir = new File("/Users/manabu/Desktop/ZenrinOUT/TXT");
         File outDir = new File("/Users/manabu/Desktop/ZenrinOUT/SHP");
-        //File outDir = new File("C:\\Users\\manab\\Dropbox\\GIS\\GISデータ\\ゼンリン\\Area Map2\\OUT\\SHP");
-        readFiles(dir, outDir, 4);
+        //readFiles(dir, outDir, 4);
+        readTxtFile(file, outDir);
     }
 
     public static void readFiles(File dir, File outDir, int thread) throws Exception {
@@ -49,90 +49,85 @@ public class ReadTxtFiles {
     }
 
 
-    public static void readTxtFile(File file, File outDir) throws Exception {
-        System.out.println("[読込]" + file.getName() + "を読み込みます。");
-        file.setReadOnly();
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "Shift_JIS"));
-        String line;
-        LinkedList<Object> featuresList = new LinkedList<Object>();
-        LinkedList<String> textName = new LinkedList<String>();
-        int figTypeI = 0;
-        int layerI = 0;
+    public static void readTxtFile(File file, File outDir) {
+        try {
+            System.out.println("[読込]" + file.getName() + "を読み込みます。");
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "Shift_JIS"));
+            String line;
+            LinkedList<Object> featuresList = new LinkedList<Object>();
+            LinkedList<String> textName = new LinkedList<String>();
+            int figTypeI = 0;
+            int layerI = 0;
 
-        while((line=br.readLine())!=null) {
-            // 空行の処理
-            if(line.equals(""))
-                ; //System.out.println("空行です");
-            else if(line.startsWith("[")) { // 上記以外の処理　[から始まるパラグラフ
-                LinkedList<Object> featureList = new LinkedList<Object>();
-                String id = "";
-                String figType = "";
-                String layer = "";
-                String dispLevel = "";
-                String no = "";
-                LinkedList<LinkedList<String>> pointList = new LinkedList<LinkedList<String>>();
-                LinkedList<String> textList = new LinkedList<String>();
+            while ((line = br.readLine()) != null) {
+                // 空行の処理
+                if (line.equals(""))
+                    ; //System.out.println("空行です");
+                else if (line.startsWith("[")) { // 上記以外の処理　[から始まるパラグラフ
+                    LinkedList<Object> featureList = new LinkedList<Object>();
+                    String id = "";
+                    String figType = "";
+                    String layer = "";
+                    String dispLevel = "";
+                    String no = "";
+                    LinkedList<LinkedList<String>> pointList = new LinkedList<LinkedList<String>>();
+                    LinkedList<String> textList = new LinkedList<String>();
 
-                while (line != null) {
-                    if (line.startsWith("[")) {
-                        id = line.substring(1, line.length() - 1);
+                    while (line != null) {
+                        if (line.startsWith("[")) {
+                            id = line.substring(1, line.length() - 1);
+                        } else if (line.startsWith("FIGTYPE")) {
+                            String pair[] = line.split("=");
+                            figType = pair[1];
+                            figTypeI = Integer.parseInt(figType);
+                        } else if (line.startsWith("DISPLEVEL")) {
+                            String pair[] = line.split("=");
+                            dispLevel = pair[1];
+                        } else if (line.startsWith("LAYER")) {
+                            String pair[] = line.split("=");
+                            layer = pair[1];
+                            layerI = Integer.parseInt(layer);
+                        } else if (line.startsWith("NO")) {
+                            String pair[] = line.split("=");
+                            no = pair[1];
+                        } else if (line.startsWith("PNTTYPE"))
+                            ;
+                        else if (line.startsWith("PNTNUM"))
+                            ;
+                        else if (line.startsWith("PNT")) {
+                            String pair[] = line.split("=");
+                            String str[] = pair[1].split(",");
+                            LinkedList<String> point = new LinkedList<String>();
+                            point.addLast(str[0]);
+                            point.addLast(str[1]);
+                            pointList.addLast(point);
+                        } else if (line.startsWith("TEXT")) {
+                            String pair[] = line.split("=");
+                            if (!textName.contains(pair[0]))
+                                textName.addLast(pair[0]);
+                            textList.addLast(pair[1]);
+                        } else if (line.equals(""))
+                            break;
+                        line = br.readLine();
                     }
-                    else if(line.startsWith("FIGTYPE")) {
-                        String pair[] = line.split("=");
-                        figType = pair[1];
-                        figTypeI = Integer.parseInt(figType);
-                    }
-                    else if(line.startsWith("DISPLEVEL")) {
-                        String pair[] = line.split("=");
-                        dispLevel = pair[1];
-                    }
-                    else if(line.startsWith("LAYER")) {
-                        String pair[] = line.split("=");
-                        layer = pair[1];
-                        layerI = Integer.parseInt(layer);
-                    }
-                    else if(line.startsWith("NO")) {
-                        String pair[] = line.split("=");
-                        no = pair[1];
-                    }
-                    else if(line.startsWith("PNTTYPE"))
-                        ;
-                    else if(line.startsWith("PNTNUM"))
-                        ;
-                    else if(line.startsWith("PNT")) {
-                        String pair[] = line.split("=");
-                        String str[] = pair[1].split(",");
-                        LinkedList<String> point = new LinkedList<String>();
-                        point.addLast(str[0]);
-                        point.addLast(str[1]);
-                        pointList.addLast(point);
-                    }
-                    else if(line.startsWith("TEXT")) {
-                        String pair[] = line.split("=");
-                        if(!textName.contains(pair[0]))
-                            textName.addLast(pair[0]);
-                        textList.addLast(pair[1]);
-                    }
-                    else if (line.equals(""))
-                        break;
-                    line = br.readLine();
+                    featureList.addLast(id);
+                    featureList.addLast(figType);
+                    featureList.addLast(dispLevel);
+                    featureList.addLast(layer);
+                    featureList.addLast(no);
+                    featureList.addLast(pointList);
+                    featureList.addLast(textList);
+                    featuresList.addLast(featureList);
                 }
-                featureList.addLast(id);
-                featureList.addLast(figType);
-                featureList.addLast(dispLevel);
-                featureList.addLast(layer);
-                featureList.addLast(no);
-                featureList.addLast(pointList);
-                featureList.addLast(textList);
-                featuresList.addLast(featureList);
             }
+            br.close();
+            createShape(file, outDir, figTypeI, layerI, featuresList, textName);
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        br.close();
-        createShape(file, outDir, figTypeI, layerI, featuresList, textName);
     }
-
     public static void createShape(File file, File outDir, int figType, int layer, LinkedList<Object> featuresList, LinkedList<String> texts) throws Exception {
-        System.out.println("[変換]" + file.getName() + "をShapeファイルに変換します。");
+        System.out.println("[変換]" + file.getName() + "をShapeファイルに変換します。: " + Thread.currentThread().getId());
         String inFileName[] = file.getName().split("\\.");
         String outFilePath = outDir.getPath() + "/" + inFileName[0] + ".shp";
         File outFile = new File(outFilePath);
@@ -201,7 +196,6 @@ public class ReadTxtFiles {
                     if(j==0)
                         cord[pointList.size()] =  new Coordinate(lon, lat);
                 }
-                System.out.println(cord);
                 Polygon polygon = geometryFactory.createPolygon(cord);
                 featureBuilder.add(polygon);
                 featureBuilder.add(featureList.get(0));
@@ -240,8 +234,8 @@ public class ReadTxtFiles {
                 SimpleFeature feature = featureBuilder.buildFeature(null);
                 features.add(feature);
             }
-
             CreateShape.createShapeFile(outFile, "UTF-8", type, features);
         }
+        System.out.println("[完了]" + file.getName() + "をShapeファイルに変換が完了しました。: " + Thread.currentThread().getId());
     }
  }
