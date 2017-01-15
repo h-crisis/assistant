9d32abb9b3dbfcf39bd1dcab5bb4f5ff89a7c25d
Manabu Ichikawa <manabu.ichikawa@ichilab.org>
2017-01-15 17:49:41 +0900

    2017.01.15 17:48 Common/src/googleapiを追加した。Google Apiを利用して施設情報を検索することができる。

---
+++ Common/src/googleapi/GoogleApi.java
@@ -0,0 +1,68 @@
+package googleapi;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created by manabu on 2017/01/14.
+ */
+public class GoogleApi {
+    private String key;
+
+    public GoogleApi() {
+
+    }
+
+    public GoogleApi(String key) {
+        this.key = key;
+    }
+
+    private StringBuilder getResponse(HttpURLConnection connection) {
+        StringBuilder builder = new StringBuilder();
+        try (InputStreamReader isr = new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8);
+             BufferedReader reader = new BufferedReader(isr)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("GoogleApi Error. Can not get input stream from connection. -getResponse-");
+            System.exit(1);
+        }
+        return builder;
+    }
+
+    public JSONObject textSearch(String str, String language) {
+        StringBuilder builder = null;
+        try {
+            URL url = new URL("https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + str +
+                    "&language=" + language + "&key=" + key);
+            HttpURLConnection connection = null;
+
+            try {
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    builder = getResponse(connection);
+                }
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JSONObject jsonObject = new JSONObject(builder.toString());
+        return jsonObject;
+    }
+}
---
+++ Common/src/googleapi/GoogleApiMain.java
@@ -0,0 +1,17 @@
+package googleapi;
+
+import org.json.JSONObject;
+
+/**
+ * Created by manabu on 2017/01/14.
+ */
+public class GoogleApiMain {
+    public static void main(String args[]) {
+        GoogleApi ga = new GoogleApi(args[0]);
+        JSONObject jsonObject = ga.textSearch("東京タワー", "ja");
+        System.out.println(GoogleApiParser.getFormattedAddress(jsonObject));
+        System.out.println(GoogleApiParser.getLon(jsonObject) + " " + GoogleApiParser.getLat(jsonObject));
+        System.out.println(GoogleApiParser.getName(jsonObject));
+        System.out.println(jsonObject);
+    }
+}
