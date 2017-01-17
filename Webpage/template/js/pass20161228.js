a348440b8cbd5f8daf249b05d56a7123813c9f8e
KKomor <kenichiro.komori@hcrisis2.onmicrosoft.com>
2017-01-17 15:28:50 +0900

    [fix]gmap adapt

--- Webpage/template/js/pass20161228.js
+++ Webpage/template/js/pass20161228.js
@@ -4,7 +4,7 @@
 
 var styleImpassable = new ol.style.Style({
     text: new ol.style.Text({
-        offsetY: 14,
+        offsetY: 8,
         font: '12px Calibri,sans-serif',
         fill: new ol.style.Fill({color: "#000000"}),
         stroke: new ol.style.Stroke({color: "#ffff00", width: 2})
