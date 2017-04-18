5d4261caf57e622dfba3652dfd2597bb3faf0505
KKomor <kenichiro.komori@hcrisis2.onmicrosoft.com>
2017-04-18 19:19:53 +0900

    [make]schadule

--- test/komori/test/jv/src/csv2csv.java
+++ test/komori/test/jv/src/csv2csv.java
@@ -1,9 +1,9 @@
 /**
- * Created by komori on 2017/04/12.
+ * Created by komori on 2017/04/18.
  */
 import java.io.*;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
+import java.nio.file.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,15 +14,23 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class test {
-    public static void main(String args[]){
+public class csv2csv {
+
+    public static void main(String args[]) {
+        File inf = new File("/Users/komori/Downloads/master.csv");
+        File ouf = new File("/Users/komori/Downloads/new_hospital_master.csv");
+        converter(inf,ouf);
+    }
+
+    public static HashMap<String, String> prefectureDB = new HashMap<>();
+
+    public static HashMap<String, String> getPefectureDB(){
 
         String tdfk[] = {"北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県", "茨城県", "栃木県", "群馬県", "埼玉県",
                 "千葉県", "東京都", "神奈川県", "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県", "静岡県", "愛知県", "三重県",
                 "滋賀県", "京都府", "大阪府", "兵庫県", "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県",
                 "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県", "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"};
 
-        HashMap<String, String> prefectureDB = new HashMap<>();
         int j = 1;
         for(String prefecture : tdfk) {
             if(j<10) {
@@ -33,23 +41,22 @@ public class test {
             }
             j++;
         }
+        return prefectureDB;
+    }
 
+
+    public static void converter(File ipf, File opf){
         int lNum = 1;
-        //Path rPath = Paths.get(args[0]);
-        //Path wPath = Paths.get(args[1]);
 
-        List<String> textList = new ArrayList<String>();
-
-        File inf = new File("/Users/komori/Downloads/master.csv");
-        File ouf = new File("/Users/komori/Downloads/new_hospital_master.csv");
         String text = "";
         try {
-            InputStream is = new FileInputStream(inf);
-            OutputStream os = new FileOutputStream(ouf);
+            InputStream is = new FileInputStream(ipf);
+            OutputStream os = new FileOutputStream(opf);
             Reader r  = new InputStreamReader(is, "SJIS");
             Writer w = new OutputStreamWriter(os, "SJIS");
             BufferedReader br = new BufferedReader(r);
             BufferedWriter bw = new BufferedWriter(w);
+            getPefectureDB();
 
             while((text=br.readLine())!=null) {
                 String array[] = text.split("\",\"");
@@ -59,7 +66,7 @@ public class test {
                 }
                 else {
                     text = "\"" + prefectureDB.get(array[6]) + "1" + array[1] + "\"," + text;
-                    bw.write("¥n" + text);
+                    bw.write("\n" + text);
                     System.out.println(text);
                 }
                 lNum++;
@@ -72,4 +79,3 @@ public class test {
         }
     }
 }
-
